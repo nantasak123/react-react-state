5d47961c85340fcb3363fee1b0cf325d9219a189
fluke <chaikhwan32@gmail.com>
2025-06-10 13:44:07 +0700

    feat : add button function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [getLanguage,setLanguage] = useState("Greeting Message")
+
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{getLanguage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => {setLanguage("สวัสดี!")}}>สวัสดี!</button>
+        <button onClick={() => {setLanguage("Hi!")}}>Hi!</button>
+        <button onClick={() =>{setLanguage("你好")}}>你好!</button>
       </div>
     </div>
   );
 }
+
 
 export default App;
